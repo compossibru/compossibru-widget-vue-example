3894510afcedfb518cb9e1c2e539e479575e8585
Maik Schmidt <maik.schmidt.hl@gmail.com>
2018-11-24 23:35:18 +0100

    add apollo-client and display all planets

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <div v-if="$apollo.loading">Loading...</div>
+        <div v-else>
+            <div v-for="(planet, index) in allPlanets.planets" :key="index">
+                {{planet.name}}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import gql from 'graphql-tag';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        apollo: {
+            allPlanets: gql`query {
+                allPlanets {
+                    planets {
+                        name
+                    }
+                }
+            }`
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueApollo);
+
+const apolloClient = new ApolloClient({
+    link: new HttpLink({
+        uri: 'https://swapi.apis.guru'
+    }),
+    cache: new InMemoryCache()
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+});
 
 export default (containerId) => {
     new Vue({
+        apolloProvider,
         render: h => h(App)
     }).$mount(`#${containerId}`)
 };
